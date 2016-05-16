732c3ea7c9f7dcb07df9105ffe049f447a9fa6e2
EC2 Default User <ec2-user@ip-10-0-0-147.localdomain>
2016-05-16 08:55:51 +0000

    php用レシピを追加

---
+++ study/recipes/php.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: study
+# Recipe:: php
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{php php-pdo php-mbstring php-mysqlnd}.each do |pkg|
+  package pkg do
+    action :install
+  end
